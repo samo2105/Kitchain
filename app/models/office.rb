dd006f346e3be06cbc6afd4c422ca0b066934c50
samo2105 <samuel.cea95@gmail.com>
2019-10-21 00:51:58 -0300

    Dashboards code reformat, model methods for finding sales done, realtime dashboard done, landing img problem solved

--- app/models/office.rb
+++ app/models/office.rb
@@ -3,4 +3,21 @@ class Office < ApplicationRecord
   has_many :workers, dependent: :nullify
   has_many :sales, dependent: :nullify
   has_many :products, dependent: :destroy
+
+  def self.find_month_sales(office)
+    date = Date.today
+    office.sales.where(:created_at => date.at_beginning_of_month..date.at_end_of_month)
+  end
+
+  def self.find_year_sales(office)
+    date = Date.today
+    office.sales.where(:created_at => date.at_beginning_of_year..date.at_end_of_year)
+  end
+
+  def self.find_today_sales(office)
+    date = Date.today
+    office.sales.where(:created_at => date.at_beginning_of_day..date.at_end_of_day)
+  end
+
+
 end
