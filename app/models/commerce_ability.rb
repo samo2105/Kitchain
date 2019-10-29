7dd9c9931cf23c06d5da04a0d07413b049492225
samo2105 <samuel.cea95@gmail.com>
2019-10-29 20:09:22 -0300

    cancancan fix

--- app/models/commerce_ability.rb
+++ app/models/commerce_ability.rb
@@ -5,8 +5,8 @@ class CommerceAbility
 
   def initialize(commerce)
       can :create, [Worker, Office, Sale, Order, Table, Product]
-      can [:edit, :delete, :update, :read], [Worker, Office], commerce_id: commerce.id
+      can [:manage], [Worker, Office], commerce_id: commerce.id
       can [:manage], [Sale, Product, Table], office_id: commerce.offices.ids
-      can [:edit, :delete, :update, :read], [Order], sale_id: Commerce.find_orders_ids(commerce)
+      can [:manage], [Order], sale_id: Commerce.find_orders_ids(commerce)
   end
 end
